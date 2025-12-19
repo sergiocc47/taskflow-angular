414e289a07303d2dc3bd6dd85ce4e44b04d17790
sergiocc47 <sergiocc47dev@gmail.com>
2025-12-19 21:34:50 +0100

    Style Login Form

--- taskflow-frontend/src/app/features/auth/components/login-form/login-form.component.ts
+++ taskflow-frontend/src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,9 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login-form',
-  imports: [ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
@@ -11,12 +25,21 @@ export class LoginFormComponent {
   @Output() login = new EventEmitter<{ email: string; password: string }>();
 
   loginForm: FormGroup;
+  hide = true;
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
       email: ['demo@taskflow.com', [Validators.required, Validators.email]],
       password: ['123456', Validators.required]
     });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
   submit() {
